02eb7caca42e87f26286c4fc6aa07a4fe746aff2
shindonghyun <donghyun5349@naver.com>
2020-10-23 12:28:30 +0900

    1022

--- practice/bj_5427_불.py
+++ practice/bj_5427_불.py
@@ -4,11 +4,16 @@ sys.stdin = open("input_data/bj_5427")
 
 
 T = int(input())
-from collections import deque
 
-def bfs(Q):
+dx = [1, -1, 0,  0]
+dy = [0,  0, 1, -1]
+
+
+def bfs(fire, people):
+    global result
+    Q = fire
     while Q:
-        y, x = Q.popleft()
+        y, x = Q.pop(0)
         for i in range(4):
             tx = x + dx[i]
             ty = y + dy[i]
@@ -19,62 +24,52 @@ def bfs(Q):
 
             Q.append([ty, tx])
             visit[ty][tx] = visit[y][x] + 1
-            if tx == 0 or tx == w - 1 or ty == 0 or ty == h - 1:
-                return visit[ty][tx]
 
-
-def bfs_fire(Q_fire):
-    while Q_fire:
-        y, x = Q_fire.popleft()
+    Q = people
+    while Q:
+        y, x = Q.pop(0)
         for i in range(4):
             tx = x + dx[i]
             ty = y + dy[i]
             if tx < 0 or tx == w or ty < 0 or ty == h:
                 continue
-            if arrs[ty][tx] == '#' or visit_fire[ty][tx] != 0:
+            if arrs[ty][tx] == '#':
                 continue
+            if visit[y][x] + 1 < visit[ty][tx]:
+                result ='impossible'
+                return
+            Q.append([ty, tx])
+            visit[ty][tx] = visit[y][x] + 1
 
-            Q_fire.append([ty, tx])
-            visit_fire[ty][tx] = visit_fire[y][x] + 1
 
-            if tx == 0 or tx == w - 1 or ty == 0 or ty == h - 1:
-                return visit_fire[ty][tx]
 
-#    동   서  남  북
-dx = [1, -1, 0,  0]
-dy = [0,  0, 1, -1]
 for test_case in range(1, T+1):
+    result = 0
     w, h = map(int, input().split())
-
     arrs = [list(input()) for _ in range(h)]
 
-    visit_fire = [ [0]*w for _ in range(h)]
-    visit = [[0] * w for _ in range(h)]
-
-
-
-
-
-
-    Q = deque()
-    Q_fire = deque()
+    Q = []
+    people = []
+    fire = []
+    goal = []
     for y in range(h):
         for x in range(w):
             if arrs[y][x] == '@':
-                Q.append([y,x])
-                visit[y][x] = 1
+                people.append([y ,x])
             elif arrs[y][x] == '*':
-                Q_fire.append([y,x])
-                visit_fire[y][x] = 1
+                fire.append([y,x])
+            elif x == 0 or x == w - 1 or y == 0 or y == h - 1:
+                goal.append([y][x])
 
+    visit = [[0] * w for _ in range(h)]
+    bfs(fire, people)
 
-
-    a = bfs(Q)
-    b = bfs_fire(Q_fire)
-
-    if not a:
-        print('IMPOSSIBLE')
-    elif a < b:
-        print(a)
-    else:
-        print('IMPOSSIBLE')
+    # for y in range(h):
+    #     for x in range(w):
+    #         if x == 0 or x == w - 1 or y == 0 or y == h - 1:
+    #             if visit[y][x]:
+    #                 print(visit[y][x])
+    print()
+    for i in visit:
+        print(*i)
+    print(result)
